4274cc4971c3764dc610619c5f7c28c696dcd9c1
z4hid <zahidhasan9297@gmail.com>
2025-02-04 19:17:44 +0600

    added search capability with serper

--- src/content_synergy/crew.py
+++ src/content_synergy/crew.py
@@ -1,9 +1,12 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool
 
 # If you want to run a snippet of code before or after the crew starts, 
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+search_tool = SerperDevTool()
 
 @CrewBase
 class ContentSynergy():
@@ -21,7 +24,8 @@ class ContentSynergy():
 	def researcher(self) -> Agent:
 		return Agent(
 			config=self.agents_config['researcher'],
-			verbose=True
+			verbose=True,
+			tools=[search_tool]
 		)
 
 	@agent
